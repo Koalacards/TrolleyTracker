fd285255bf909d2dcbfb50d43e03c1dd888da920
Hayden <44925247+Koalacards@users.noreply.github.com>
2022-01-31 11:55:30 -0500

    Trolley v5.4.2: Add booter file

---
+++ Bot/booter.py
@@ -0,0 +1,19 @@
+import subprocess
+import asyncio
+import tracemalloc
+
+tracemalloc.start()
+
+while True:
+    p = subprocess.Popen("python3 bot.py", shell=True).wait()
+
+    if p!= 0:
+        continue
+    else:
+        print("trolley stopped")
+        snap = tracemalloc.take_snapshot()
+        top_stats = snap.statistics('lineno')
+        print(f"[Top 10]")
+        for stat in top_stats[:10]:
+            print(stat)
+        asyncio.sleep(300)
